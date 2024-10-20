953984eaa68349fdb54b65e657b8f8e91f57d724
Akshat <gameratom1505@gmail.com>
2024-10-20 17:02:38 +0530

    Full Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-import Login from './assets/Components/Login'
-import { BrowserRouter as Router, Routes ,Route} from 'react-router-dom'
-import Dashboard from './assets/Components/Dashboard' 
-
-
+import { useState } from "react";
+import "./App.css";
+import Login from "./assets/Components/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./assets/Components/Dashboard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/assets/Components/Login.jsx
+++ src/assets/Components/Login.jsx
@@ -1,43 +1,42 @@
-import React from 'react'
-import {signInWithEmailAndPassword} from "firebase/auth"
-import {auth} from "../../firebase/firebase"
-import './Login.css'
-import {useNavigate} from "react-router-dom"
+import React from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/firebase";
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  let navigate = useNavigate();
 
-  let navigate=useNavigate()
-
-  const handleSubmit = async (e)=>{
-    e.preventDefault()
-    const email=e.target[0].value
-    const pass=e.target[1].value
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const pass = e.target[1].value;
     try {
-      const resp=await signInWithEmailAndPassword(auth,email,pass)
-      console.log(resp)
-      navigate('/dashboard')
+      const resp = await signInWithEmailAndPassword(auth, email, pass);
+      console.log(resp);
+      navigate("/dashboard");
     } catch (error) {
       console.log(error);
-      
     }
-  }
+  };
 
   return (
-  <div className='login-page'>  
-    <div className='wrapper'>
-      <form onSubmit={handleSubmit}>
-        <h1>Login</h1>
-        <div className='emailBox'>
-            <input type="email" placeholder='Your Email' required />
-        </div>
-        <div className='passBox'>
-            <input type="password" placeholder='Your Password' required />
-        </div>
-        <button>Login</button>
-      </form>
+    <div className="login-page">
+      <span className="welcome">Welcome To KPI DASHBOARD</span>
+      <div className="wrapper">
+        <form onSubmit={handleSubmit}>
+          <h1>LOGIN</h1>
+          <div className="emailBox">
+            <input type="email" placeholder="Your Email" required />
+          </div>
+          <div className="passBox">
+            <input type="password" placeholder="Your Password" required />
+          </div>
+          <button>Login</button>
+        </form>
+      </div>
     </div>
-  </div>  
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
