07945854dbb0b2e9d191c3fd7fad38285cdbaf75
Akshat Vidyarthi <154006778+Akshat1505@users.noreply.github.com>
2024-10-25 00:26:36 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default {
+export default defineConfig({
+  base: './',  // Important for relative pathing
+  plugins: [react()],
   build: {
     rollupOptions: {
       external: ['react-router-dom']
     }
   }
-};
+});
