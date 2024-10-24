5efda277bdf28ae49f4ee3c4f6e93a16021cc91b
Akshat Vidyarthi <154006778+Akshat1505@users.noreply.github.com>
2024-10-24 15:45:41 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from "react";
 import "./App.css";
 import Login from "./assets/Components/Login";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Form } from "react-router-dom";
 import Dashboard from "./assets/Components/Dashboard";
+import ForgotPassword from "./assets/Components/ForgotPassword";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/reset" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
